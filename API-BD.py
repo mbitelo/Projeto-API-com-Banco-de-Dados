58c7a4d110f7aeab81a85a17e099ad181f52efef
XxAllissonxX <60507697+XxAllissonxX@users.noreply.github.com>
2020-02-04 11:55:38 -0300

    Update API-BD.py

--- API-BD.py
+++ API-BD.py
@@ -12,34 +12,45 @@ conexao = mysql.connector.connect(
 )
 cursor = conexao.cursor(buffered=True)
 
-cursor.execute("select id_veiculo from todos_veiculos limit 5")
+#fazendo consulta na tabela veiculos e selecionado os ids, Ao todo são 739 ids, mas colocquei os 5 primeiros.
+cursor.execute("select id_veiculo from veiculos limit 5")
 
 data_inicio = input( 'Digite a data de inicio que deseja consultar (ex: AAAA-MM-DD):' )
 data_fim = input( 'Digite a data de fim que deseja consultar (ex: AAAA-MM-DD): ' )
 
+#Aqui armazeno os 5 ids da consulta nessa lista (veiculo =[])
 veiculos = []
 for x in cursor:
     veiculos.append(x)
     
+    #Aqui pego os 5 ids e jogo na url da api para pega os ddos de json de cada id.
     for y in veiculos:
         req = requests.get(
             f'http://backend.rassystem.com.br/app/v1/api/veiculos/{y[0]}/rastreio?dataInicio={data_inicio}T12:00:00-03:00&dataFim={data_fim}T12:00:00-03:00',
             auth=('aboliveira', 'alison00'))
         dados = json.loads(req.text)
         
+        #Aqui intero os resultados do json, so preciso desses 4 dados da api (idveiculo, data, latitude e longitude)
         for i in dados['features']:
             idveiculo = i['properties']['idVeiculo']
-            print('IdVeiculo...:', idveiculo)
+            #print('IdVeiculo...:', idveiculo)
             data = i['properties']['data']
-            print('Data...:', data)
+            #print('Data...:', data)
             latitude = i['geometry']['coordinates'][0]
-            print('Latitude...:', latitude)
+            #print('Latitude...:', latitude)
             longitude = i['geometry']['coordinates'][1]
-            print('Longitude...:', longitude)
-            print('\n')
+            #print('Longitude...:', longitude)
+            #print('\n')
             
-            query = (
-                'INSERT INTO log2(idveiculo, datas, latitude, longitude) VALUES ("%s", %s, "%s", "%s")')
-            var = idveiculo, data, latitude, longitude
-            cursor.execute(query, var)
+            #Faço a inserção dos dados em outra tabela (log2)
+            insert_query = "INSERT INTO log2(idveiculo, datas, latitude, longitude) VALUES ('" + str(
+                idveiculo ) + "','" + data + "','" + str( latitude ) + "','" + str( longitude ) + "')"
+            print( insert_query )
+            cursor.execute(insert_query)
+            
             conexao.commit()
+            
+            
+            #OBS: Quando coloco para imprimir o insert_query, todos os 5 ids são impressos na tela.
+            #Mas se colocar o cursor.execute(insert_qery) com ta ai, so imprimi o 1º id e so salva esse 1º no banco.
+            #Como fazer para imprimir todos os ids que eu coocar la na consulta?
